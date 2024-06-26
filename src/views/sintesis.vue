b2c3eb08fdce223daed513910e7fff337bd9b8dd
emilydev <98632409+emilydev@users.noreply.github.com>
2024-06-26 18:36:42 -0500

    se sube texto alternativo de la sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -7,7 +7,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="La figura es un diagrama que explica el manejo de bases de datos relacionales con SQL. Detalla las secciones de Comandos Básicos de SQL, DML (Data Manipulation Language), Funciones avanzadas, Optimización y mantenimiento, e Interfaces y herramientas. Explica operaciones como crear, consultar, modificar y eliminar bases de datos y datos, así como el uso de funciones avanzadas como claves primarias, índices y restricciones. También aborda temas de optimización, como la indexación y partición de tablas, y menciona herramientas como MySQL Workbench y API para la integración con otros lenguajes como PHP.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
